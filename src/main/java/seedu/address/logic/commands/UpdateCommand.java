c449d3dcb6dd846f6f2f9757d3430b9d264c9d30
caiwang0-admin <kelvincaiwang@gmail.com>
2025-10-12 19:01:51 +0800

    Implement update command

--- src/main/java/seedu/address/logic/commands/UpdateCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateCommand.java
@@ -1,74 +1,86 @@
-//package seedu.address.logic.commands;
-//
-//import static java.util.Objects.requireNonNull;
-//
-//import java.util.List;
-//
-//import seedu.address.commons.core.index.Index;
-//import seedu.address.logic.Messages;
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.Model;
-//import seedu.address.model.Company.InternshipApplication;
-//
-///**
-// * Updates the status of an existing person in the address book.
-// */
-//public class UpdateCommand extends Command {
-//
-//    public static final String COMMAND_WORD = "update";
-//
-//    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the status of the person identified "
-//            + "by the index number used in the displayed person list. "
-//            + "Existing status will be overwritten by the input values.\n"
-//            + "Parameters: INDEX (must be a positive integer) "
-//            + "s/STATUS\n"
-//            + "Example: " + COMMAND_WORD + " 1 "
-//            + "s/Interviewing";
-//
-//    public static final String MESSAGE_UPDATE_PERSON_SUCCESS = "Status updated for '%1$s' to: %2$s";
-//
-//    private final Index index;
-//    private final String status;
-//
-//    /**
-//     * @param index of the person in the filtered person list to update
-//     * @param status of the person to be updated to
-//     */
-//    public UpdateCommand(Index index, String status) {
-//        requireNonNull(index);
-//        requireNonNull(status);
-//
-//        this.index = index;
-//        this.status = status;
-//    }
-//
-//    @Override
-//    public CommandResult execute(Model model) throws CommandException {
-//        requireNonNull(model);
-//        List<Person> lastShownList = model.getFilteredPersonList();
-//
-//        if (index.getZeroBased() >= lastShownList.size()) {
-//            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//        }
-//
-//        Person personToUpdate = lastShownList.get(index.getZeroBased());
-//        // For now, just return a success message without actually updating the person
-//        return new CommandResult(String.format(MESSAGE_UPDATE_PERSON_SUCCESS, personToUpdate.getName(), status));
-//    }
-//
-//    @Override
-//    public boolean equals(Object other) {
-//        if (other == this) {
-//            return true;
-//        }
-//
-//        // instanceof handles nulls
-//        if (!(other instanceof UpdateCommand)) {
-//            return false;
-//        }
-//
-//        UpdateCommand otherUpdateCommand = (UpdateCommand) other;
-//        return index.equals(otherUpdateCommand.index)
-//                && status.equals(otherUpdateCommand.status);
-//    }
-//}
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.Company.InternshipApplication;
+import seedu.address.model.ApplicationStatus.ApplicationStatus;
+
+/**
+ * Updates the status of an existing internship application in the address book.
+ */
+public class UpdateCommand extends Command {
+
+    public static final String COMMAND_WORD = "update";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates the status of the internship application identified "
+            + "by the index number used in the displayed application list. "
+            + "Existing status will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "s/STATUS\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "s/Interviewing";
+
+    public static final String MESSAGE_UPDATE_APPLICATION_SUCCESS = "Status updated for '%1$s' to: %2$s";
+
+    private final Index index;
+    private final ApplicationStatus status;
+
+    /**
+     * @param index of the internship application in the filtered application list to update
+     * @param status of the application to be updated to
+     */
+    public UpdateCommand(Index index, ApplicationStatus status) {
+        requireNonNull(index);
+        requireNonNull(status);
+
+        this.index = index;
+        this.status = status;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<InternshipApplication> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        InternshipApplication applicationToUpdate = lastShownList.get(index.getZeroBased());
+        InternshipApplication updatedApplication = new InternshipApplication(
+                applicationToUpdate.getName(),
+                applicationToUpdate.getIndustry(),
+                applicationToUpdate.getJobType(),
+                applicationToUpdate.getDescription(),
+                status,
+                applicationToUpdate.getEmail());
+
+        model.setPerson(applicationToUpdate, updatedApplication);
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_UPDATE_APPLICATION_SUCCESS, 
+                applicationToUpdate.getName(), status));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof UpdateCommand)) {
+            return false;
+        }
+
+        UpdateCommand otherUpdateCommand = (UpdateCommand) other;
+        return index.equals(otherUpdateCommand.index)
+                && status.equals(otherUpdateCommand.status);
+    }
+}
--- src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UpdateCommandParser.java
@@ -1,43 +1,69 @@
-//package seedu.address.logic.parser;
-//
-//import static java.util.Objects.requireNonNull;
-//import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
-//
-//import seedu.address.commons.core.index.Index;
-//import seedu.address.commons.exceptions.IllegalValueException;
-//import seedu.address.logic.commands.UpdateCommand;
-//import seedu.address.logic.parser.exceptions.ParseException;
-//
-///**
-// * Parses input arguments and creates a new UpdateCommand object
-// */
-//public class UpdateCommandParser implements Parser<UpdateCommand> {
-//
-//    /**
-//     * Parses the given {@code String} of arguments in the context of the UpdateCommand
-//     * and returns an UpdateCommand object for execution.
-//     * @throws ParseException if the user input does not conform the expected format
-//     */
-//    public UpdateCommand parse(String args) throws ParseException {
-//        requireNonNull(args);
-//        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_STATUS);
-//
-//        Index index;
-//        try {
-//            index = ParserUtil.parseIndex(argMultimap.getPreamble());
-//        } catch (IllegalValueException ive) {
-//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-//                    UpdateCommand.MESSAGE_USAGE), ive);
-//        }
-//
-//        if (!argMultimap.getValue(PREFIX_STATUS).isPresent()) {
-//            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-//                    UpdateCommand.MESSAGE_USAGE));
-//        }
-//
-//        String status = argMultimap.getValue(PREFIX_STATUS).get();
-//
-//        return new UpdateCommand(index, status);
-//    }
-//}
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.UpdateCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.ApplicationStatus.ApplicationStatus;
+
+/**
+ * Parses input arguments and creates a new UpdateCommand object
+ */
+public class UpdateCommandParser implements Parser<UpdateCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the UpdateCommand
+     * and returns an UpdateCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public UpdateCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_STATUS);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UpdateCommand.MESSAGE_USAGE), ive);
+        }
+
+        if (!argMultimap.getValue(PREFIX_STATUS).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    UpdateCommand.MESSAGE_USAGE));
+        }
+
+        String statusString = argMultimap.getValue(PREFIX_STATUS).get();
+        
+        // Convert to proper case for validation
+        String normalizedStatus = normalizeStatus(statusString);
+        if (!ApplicationStatus.isValidStatus(normalizedStatus)) {
+            throw new ParseException(ApplicationStatus.MESSAGE_CONSTRAINTS);
+        }
+
+        ApplicationStatus status = new ApplicationStatus(normalizedStatus);
+
+        return new UpdateCommand(index, status);
+    }
+
+    /**
+     * Normalizes the status string to the proper case by matching against valid statuses.
+     * This allows case-insensitive input while maintaining the correct capitalization.
+     */
+    private String normalizeStatus(String statusInput) {
+        String[] validStatuses = ApplicationStatus.VALID_STATUSES;
+        
+        for (String validStatus : validStatuses) {
+            if (validStatus.equalsIgnoreCase(statusInput)) {
+                return validStatus;
+            }
+        }
+        
+        // If no match found, return original input (will be caught by validation)
+        return statusInput;
+    }
+}
